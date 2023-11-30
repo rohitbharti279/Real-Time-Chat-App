c8bc5ba5bcfdf09585d682de019eaeefcbdbf8b7
Rohit Bharti <rohitbharti279@gmail.com>
2023-11-30 22:22:16 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: ["http://localhost:8080"],
+    },
+})
+io.on("connection", socket => {
+    console.log("2")
+    console.log(socket.id)
+})
